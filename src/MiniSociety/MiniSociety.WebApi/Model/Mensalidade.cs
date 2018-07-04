5b61a6c1f4ed404a52b567deb8c3d3366f75c1b5
Gabriel Schmitt Kohlrausch <gabriel@society.com.br>
2018-07-04 18:29:29 -0300

    criado controller para mensalidades

---
+++ src/MiniSociety/MiniSociety.WebApi/Model/Mensalidade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MiniSociety.WebApi.Model
+{
+    public class Mensalidade
+    {
+        public int IdInscricao { get; set; }
+        public DateTime Vencimento { get; set; }
+    }
+}
