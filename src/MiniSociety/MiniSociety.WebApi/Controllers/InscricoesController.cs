6261db98ef247dc226b4cc7fbe914fe899aea162
Gabriel Schmitt Kohlrausch <gabriel@society.com.br>
2018-07-04 20:01:34 -0300

    Gerar 12 mensalidades e controlar limite de idade na turma

--- src/MiniSociety/MiniSociety.WebApi/Controllers/InscricoesController.cs
+++ src/MiniSociety/MiniSociety.WebApi/Controllers/InscricoesController.cs
@@ -1,8 +1,11 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using MiniSociety.WebApi.Infra;
 using MiniSociety.WebApi.Model;
+using System;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 
 namespace MiniSociety.WebApi.Controllers
 {
@@ -28,28 +31,41 @@ namespace MiniSociety.WebApi.Controllers
         [HttpPost]
         public IActionResult Inserir([FromBody] Inscricao inscricao)
         {
-            var sql = "INSERT INTO Inscricoes (IdAluno, IdTurma, ValorMensal) VALUES (@IdAluno, @IdTurma, @ValorMensal)";
+            var sql = "INSERT INTO Inscricoes (IdAluno, IdTurma, ValorMensal) VALUES (@IdAluno, @IdTurma, @ValorMensal); SELECT SCOPE_IDENTITY();";
             using (var conexao = new SqlConnection(@"Data Source = DRACO-VM\SQLEXPRESS2012_2; Initial Catalog = TesteAulaGabi; User ID = sa; Password = STI000;"))
             {
                 var aluno = conexao
-                                .Query<dynamic>("SELECT Id, Nome FROM Aluno WHERE Id = @IdAluno", new { inscricao.IdAluno })
+                                .Query<dynamic>("SELECT Id, Nome, DataNascimeto AS 'DataNascimento' FROM Alunos WHERE Id = @IdAluno", new { inscricao.IdAluno })
                                 .FirstOrDefault();
                 if (aluno == null)
                     return BadRequest("Aluno inválido");
 
                 var turma = conexao
-                                .Query<dynamic>("SELECT Disponivel, IdModalidade FROM Turmas WHERE Id = @Id", new { Id = inscricao.IdTurma })
+                                .Query<dynamic>("SELECT Disponivel, IdModalidade, IdadeMinima, IdadeMaxima FROM Turmas WHERE Id = @Id", new { Id = inscricao.IdTurma })
                                 .FirstOrDefault();
                 if (turma == null)
                     return BadRequest("Turma inválida");
                 if ((bool)turma.Disponivel == false)
                     return BadRequest("Turma indisponivel");
+                var idade = ((DateTime) aluno.DataNascimento).GetAge();
+                if (idade < turma.IdadeMinima || idade > turma.IdadeMaxima)
+                    return BadRequest("Fora da faixa de idade para a turma.");
+
 
                 var valorMensal = conexao
-                                    .Query<decimal>("SELECT ValorMensal FROM Modalidade WHERE Id = @Id", new { Id = turma.IdModalidade })
+                                    .Query<decimal>("SELECT ValorMensal FROM Modalidades WHERE Id = @Id", new { Id = turma.IdModalidade })
                                     .FirstOrDefault();
                 
-                var resultado = conexao.Execute(sql, new { inscricao.IdAluno, inscricao.IdTurma, ValorMensal = valorMensal });
+                var idInscricao = conexao.Query<int>(sql, new { inscricao.IdAluno, inscricao.IdTurma, ValorMensal = valorMensal }).First();
+
+                var sqlMensalidade = new StringBuilder();  
+                for (int i = 0; i < 12; i++)
+                {
+                    var vencimento = DateTime.Now.AddMonths(i);
+                    sqlMensalidade.Append($"INSERT INTO Mensalidades(idInscricao, vencimento, valor) VALUES ({idInscricao}, '{vencimento.ToString("yyyy-MM-ddTHH:mm:ss")}', {valorMensal});");
+                }
+                var result = conexao.Execute(sqlMensalidade.ToString());
+
                 return Ok();
             }
         }
---
+++ src/MiniSociety/MiniSociety.WebApi/Infra/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniSociety.WebApi.Infra
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetAge(this DateTime date)
+        {
+            var diferenca = DateTime.Today - date;
+            return (new DateTime() + diferenca).AddYears(-1).AddDays(-1).Year;
+        }
+    }
+}
